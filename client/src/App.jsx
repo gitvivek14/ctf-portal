ad9d25af048ddeb54638d4ff94121325fde1b9af
gitvivek <varora2004@gmail.com>
2024-01-31 00:55:24 +0530

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from "./components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button>Click me</Button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
