a2feaa963eff9ea6ed12fe89c3ac3744c4d06157
gitvivek <varora2004@gmail.com>
2024-02-04 01:35:51 +0530

    dontplay

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,9 +13,29 @@ exports.login_post = asyncHandler(async (req, res) => {
             return res.status(404).json({ message: 'User not found' });
         }
         if (user.password !== password) {
-            return res.status(401).json({success: false , message: 'Invalid password' });
+            return res.status(401).json({ message: 'Invalid password' });
         }
-        res.status(200).json({ message: 'Login successful' });
+        // create token
+        const payload = {
+            email:email,
+            id:user._id,
+        }
+        const token = jwt.sign(payload,process.env.JWT_SECRET,{
+            expiresIn:"1d"
+        })
+        user.token = token;
+        const options = {
+            expires: new Date(Date.now()+3*24*60*60*1000),
+                httpOnly:true
+        }
+        res.cookie("token",token,options).status(200).json({
+            success:true,
+            message:"LOGGED IN SUCCESFULLY",
+                user,
+                payload
+        })
+        // passeord ?null
+
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
     }
