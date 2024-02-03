0a84a08ca4fccc3f21a6c08ef2e648c7d0280941
sarthaksri <sarthaksri017@gmail.com>
2024-02-04 01:06:29 +0530

    ss

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -10,9 +10,9 @@ exports.login_post = asyncHandler(async (req, res) => {
             return res.status(404).json({ message: 'User not found' });
         }
         if (user.password !== password) {
-            return res.status(401).json({ message: 'Invalid password' });
+            return res.status(401).json({success: false , message: 'Invalid password' });
         }
-        res.status(200).json({ message: 'Login successful' });
+        res.status(200).json({success: false , message: 'Login successful' });
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
     }
@@ -60,9 +60,3 @@ exports.signup = async (req,res)=>{
         
     }
 }
-
-exports.register_post = asyncHandler(async (req,res) => {
-
-})
-
-
