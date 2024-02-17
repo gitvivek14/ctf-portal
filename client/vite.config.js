eccf3fa7769ff617caa33a0ce2bb54abea90345c
gitvivek14 <118727613+gitvivek14@users.noreply.github.com>
2024-02-17 15:39:29 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react-swc'
-  import path from "path"
+// <<<<<<< HEAD
+//   import { defineConfig } from 'vite'
+//   import react from '@vitejs/plugin-react-swc'
+//   import path from "path"
 
-  // https://vitejs.dev/config/
-  export default defineConfig({
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+//   // https://vitejs.dev/config/
+//   export default defineConfig({
+//     plugins: [react()],
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(__dirname, "./src"),
+//       },
 =======
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react-swc'
