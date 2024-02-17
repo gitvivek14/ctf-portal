b2915e3804cb459fcd316168933b26ff1d7b41d6
gitvivek <varora2004@gmail.com>
2024-02-17 13:14:44 +0530

    f

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  rollupOptions: {
+    input: 'src/main.js' // Adjust this path as per your project structure
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
