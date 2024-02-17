f9e5cbbe7fef2ce51c80bbac1939e41ac76d82fb
gitvivek <varora2004@gmail.com>
2024-02-17 12:39:11 +0530

    f

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import Auth from './pages/Auth'
-import {Route,Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import Home from "../src/pages/Home"
 import Level from './pages/levels/Level'
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 import DashBoard from './pages/Dashboard/DashBoard'
 import ZeroLevel from './pages/levels/ZeroLevel'
+import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
@@ -17,22 +18,31 @@ function App() {
   // })
   return (
     <>
-    <div className="w-[100vw] h-[100vh] min-h-screen bg-[#000814] overflow-y-auto bghome ">
-      <Routes>
-        <Route path='/' element={<Auth></Auth>}>
-        </Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<SignUp/>}>
-        </Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='level/:id' element={<Level></Level>}></Route>
-        <Route path='dash' element={<DashBoard></DashBoard>}></Route>
-        <Route path='/home/level/0' element={<ZeroLevel/>}></Route>
-      </Routes>
-      
+      <div className="w-[100vw] h-[100vh] min-h-screen bg-[#000814] overflow-y-auto bghome ">
+        <Routes>
+          <Route path='/' element={<Auth></Auth>}>
+          </Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/signup' element={<SignUp />}>
+          </Route>
 
-    </div>
-      
+          <Route path='/home' element={
+            <PrivateRoute>
+              <Home></Home>
+            </PrivateRoute>
+          }></Route>
+          <Route path='level/:id' element={
+            <PrivateRoute>
+              <Level></Level>
+            </PrivateRoute>
+          }></Route>
+          <Route path='dash' element={<DashBoard></DashBoard>}></Route>
+          <Route path='/home/level/0' element={<ZeroLevel />}></Route>
+        </Routes>
+
+
+      </div>
+
     </>
   )
 }
