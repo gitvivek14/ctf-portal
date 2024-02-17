e77cb8bcf6d8a213fb7fbce5053eef69553f2253
gitvivek <varora2004@gmail.com>
2024-02-17 13:16:48 +0530

    he

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import path from "path"
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   rollupOptions: {
+//     input: 'src/main.js' // Adjust this path as per your project structure
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
@@ -5,12 +21,14 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    input: 'src/main.js' // Adjust this path as per your project structure
+  build: {
+    rollupOptions: {
+      input: 'src/main.js' // Adjust this path as per your project structure
+    },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(process.cwd(), "./src"),
     },
   },
 })
